6a1eb82e878a5110d8d7504c42e9828994a6fdb8
MrZebarth <kris.zebarth@ocsb.ca>
2017-09-18 15:31:18 -0400

    Win condition
    You can now win the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
     private Rigidbody rb;
+    private int count;
+    public Text countText;
+    public Text winText;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -26,6 +33,18 @@ public class PlayerController : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
         }
     }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 8)
+        {
+            winText.text = ("You Win!");
+        }
+    }
+
 }
